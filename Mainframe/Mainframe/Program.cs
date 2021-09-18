1981b45d18f8149ca020f00f00698e7acddee39b
jas-pufund@wp.pl <jas-pufund@wp.pl>
2021-09-18 23:09:43 +0200

    0.0

--- Mainframe/Mainframe/Program.cs
+++ Mainframe/Mainframe/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Mainframe
 {
@@ -10,19 +13,27 @@ namespace Mainframe
     {
         static void Main(string[] args)
         {
-            //thdtgd
+            //Wiadomość początkowa
+            Console.WriteLine("copyright: J.Pufund,D.Trzciński");
+            Console.WriteLine("Server versja:"+Define.versja);
+
+            //Skaner UDP
+            string input=UDPListener.StartListner();
+
+            //wypisanie odebranych danych
+            Console.WriteLine(input);
         }
     }
 
-    public class UDPListener
+    public class UDPListener    //Funkcja skanera UDP
     {
-        private const int port = 10000;//port komunikacyjny
-        public static string od_adres;
-        public static int od_port;
-        public static void StartListner()
+        
+        public static string od_adres;  //Adrres nadawcy
+
+        public static string StartListner()
         {
-            UdpClient lisnter = new UdpClient(port);
-            IPEndPoint grupeEP = new IPEndPoint(IPAddress.Any, port);
+            UdpClient lisnter = new UdpClient(Define.port);
+            IPEndPoint grupeEP = new IPEndPoint(IPAddress.Any, Define.port);
 
             //bufor
             byte[] bytes = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -30,11 +41,10 @@ namespace Mainframe
             //nasłuch
             try
             {
-                while (bytes[0] == 0)
+                while (bytes[0] == 0)   //do momętu odebrania 
                 {
                     bytes = lisnter.Receive(ref grupeEP); // odebrane dane
                     od_adres = grupeEP.Address.ToString(); //pobierane ip
-                    od_port = grupeEP.Port; //pobieranie portu
                 }
             }
             catch (SocketException e)
@@ -47,7 +57,13 @@ namespace Mainframe
             }
             //zapis do pliku buforującego
             string a = System.Text.Encoding.UTF8.GetString(bytes);
-            Console.WriteLine(a);
+            //Console.WriteLine(a);
+            return a;
         }
+    }
+    public class Define //Stałe programowe
+    {
+        public const string versja="0.0";
+        public const int port = 10000;//port komunikacyjny
     }
 }
